03c49abc262e4d97d61ca4011aaf9440dd1261c6
Dmitry Matskello <dimka_matsi@hotmail.com>
2019-10-21 01:52:14 +0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,16 @@
-const gulp = require('gulp');
+'use strict';
 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
+});
